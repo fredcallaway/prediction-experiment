2af27f7f35c4b48f12d912ab85c5f69cfa597015
fredcallaway <fredc@llaway.com>
2025-12-10 09:06:18 -0500

    rm default experiment/instructions

--- components/Experiment.vue
+++ components/Experiment.vue
@@ -28,50 +28,7 @@ const correct = ref(false)
   <ESequence name="experiment" ref="experiment">
     <EConsent> <ConsentContent /> </EConsent>
   
-    <EInstructions mt-15 v-slot="{ goNext }">
-
-      <EPage>
-        <header>
-          In this experiment, you will click buttons. For example, the one below.
-        </header>
-        <PButton value="Start" @click="goNext" />
-      </EPage>
-
-    </EInstructions>
-  
-    <ERepeat name="main" :count="trials.length" v-slot="{ step }">
-      <!-- show bonus at the top left -->
-      <div text-xl font-bold>
-        Bonus: ${{ bonus.dollars.toFixed(2) }}
-      </div>
-      <!-- a trial is a button choice followed by feedback -->
-      <!-- note: you will usually create a separate component for your trials -->
-      <ESequence name="trial" text-center mt30 >
-
-        <EPage name="choice" v-slot="{ done }">
-          <div>Click the {{ trials[step].color }} button.</div>
-          <div mt3 flex-center gap4>
-            <PButton v-for="color in colors" :key="color" :value="color" :color="color" @click="() => {
-              correct = color == trials[step].color
-              if (correct) {
-                bonus.addPoints(1)
-              }
-              done()
-            }" />
-          </div>
-        </EPage>
-        
-        <EDelay name="feedback" :ms="1000" text-3xl>
-          <div text-green v-if="correct">
-            correct!
-          </div>
-          <div text-red v-else>
-            incorrect.
-          </div>
-        </EDelay>
-        
-      </ESequence>
-    </ERepeat>
+    <ESeqPredict />
   
     <ESurvey />
   
