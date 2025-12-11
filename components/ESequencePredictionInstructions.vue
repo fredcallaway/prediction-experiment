146d3dbd6c12994a3c7268acb8659f144721312d
fredcallaway <fredc@llaway.com>
2025-12-11 08:59:14 -0500

    instructions

--- components/ESequencePredictionInstructions.vue
+++ components/ESequencePredictionInstructions.vue
@@ -1,15 +1,29 @@
 <script lang="ts" setup>
-import { hooks } from './ESequencePrediction.vue'
+import { hooks, provideSequencePredictionParams } from './ESequencePrediction.vue'
 const { sleep } = useLocalAsync()
+
+const E = useEpoch('Instructions')
+
+provideSequencePredictionParams({
+  length: 2,
+  feedbackInTime: 2000,
+})
 
 const prompt = ref('Pick a side: LEFT or RIGHT')
 
+const showGame = ref(true)
+const bonus = useBonus()
+
 onMounted(async () => {
+  prompt.value = 'Want to play a game?'
+  await until(showGame).toBe(true)
+
+  prompt.value = 'Pick a side: F (left) or J (right)'
   await hooks.afterChoice.receive(async (state) => {
     state.target = state.prediction!
-    prompt.value = 'You found a coin!'
+    prompt.value = `You found a coin! Each coin is worth ${bonus.centsPerPoint} cents.`
   })
-  await sleep(2000)
+  await sleep(2500)
   prompt.value = 'Try again!'
 
   await hooks.afterChoice.receive(async (state) => {
@@ -23,8 +37,9 @@ onMounted(async () => {
 <template>
   <div>
     <div text-xl text-center t0 wfull pt13>{{ prompt }}</div>
-    <ESequencePrediction
-      :params="{ length: 2 }"
-    />
+    <div v-if="!showGame" wfull flex-center t10>
+      <PButton value="Yes" @click="showGame = true" />
+    </div>
+    <ESequencePrediction v-else />
   </div>
 </template>
