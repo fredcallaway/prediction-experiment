a183b7f063dff2587c4c52f6f78b7c7dfc803078
fredcallaway <fredc@llaway.com>
2025-12-11 19:30:09 -0500

    update epoch.config.ts (whoops!)

--- epoch.config.ts
+++ epoch.config.ts
@@ -1,9 +1,9 @@
 export default defineEpochConfig({
-  url: "https://vue-experiment-template.web.app/",
-  title: 'Psychology Experiment',
+  url: "https://coin-game-53.web.app/",
+  title: 'Coin Game',
   icon: '/favicon.svg',
   version: "v1.0",
-  contactEmail: 'yourEmailAddress@fakeDomain.foo',
+  contactEmail: 'fredcallaway@gmail.com',
   completion: {
     mode: 'prolific',
   },
