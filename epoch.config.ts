c59f9210dd7362d326053d53ba119c6ef6dc9716
fredcallaway <fredc@llaway.com>
2025-12-11 20:47:31 -0500

    v1.1

--- epoch.config.ts
+++ epoch.config.ts
@@ -2,7 +2,7 @@ export default defineEpochConfig({
   url: "https://coin-game-53.web.app/",
   title: 'Coin Game',
   icon: '/favicon.svg',
-  version: "v1.0",
+  version: "v1.1",
   contactEmail: 'fredcallaway@gmail.com',
   completion: {
     mode: 'prolific',
