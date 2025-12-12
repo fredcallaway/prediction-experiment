1d101ae40b2c1673ee701cb43b03e332af95d17f
fredcallaway <fredc@llaway.com>
2025-12-11 20:47:29 -0500

    prolific

--- prolific.config.ts
+++ prolific.config.ts
@@ -2,10 +2,10 @@ export default defineProlificConfig({
   "projectId": "693b5ae917f205af5b0c2d34",
   "name": "Coin Game",
   "description": "Earn a bonus (up to $0.50) by making accurate predictions in this quick-and-easy experiment!",
-  "estimated_completion_time": 5,
+  "estimated_completion_time": 2,
   "maximum_allowed_time": 60,
-  "reward": 100,
-  "total_available_places": 5,
+  "reward": 30,
+  "total_available_places": 27,
   "eligibility": {
     "allowUK": true,
     "minSubmissions": 50,
