c70ad39cb26543cc2e51bd128c50f541ec89720b
fredcallaway <fredc@llaway.com>
2026-01-04 13:24:56 -0800

    v3.2 with new FSM

--- epoch.config.ts
+++ epoch.config.ts
@@ -1,7 +1,7 @@
 export default defineEpochConfig({
   title: 'Coin Game',
   icon: '/favicon.svg',
-  version: "v3.1",
+  version: "v3.2",
   contactEmail: 'fredcallaway@gmail.com',
   completion: {
     mode: 'prolific',
