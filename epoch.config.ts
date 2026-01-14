2c4de8720c3c8202491108d567446379d454eb1f
fredcallaway <fredc@llaway.com>
2026-01-14 12:12:47 -0500

    POST two-gen4

--- epoch.config.ts
+++ epoch.config.ts
@@ -1,7 +1,7 @@
 export default defineEpochConfig({
   title: 'Coin Game',
   icon: '/favicon.svg',
-  version: "two-gen3",
+  version: "two-gen4",
   contactEmail: 'fredcallaway@gmail.com',
   completion: {
     mode: 'prolific',
