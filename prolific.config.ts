4cca418fb3f199e41631fefec9ef5e544a1f7827
fredcallaway <fredc@llaway.com>
2026-01-15 15:26:30 -0500

    POST two-rand

--- prolific.config.ts
+++ prolific.config.ts
@@ -6,7 +6,7 @@ export default defineProlificConfig({
   "estimated_completion_time": 2,
   "maximum_allowed_time": 60,
   "reward": 30,
-  "total_available_places": 100,
+  "total_available_places": 20,
   "eligibility": {
     "allowUK": true,
     "minSubmissions": 50,
